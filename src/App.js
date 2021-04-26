c2fb0d158fdf0f4570d14161821164f34489d0c3
jiyoon0605 <ljy014@naver.com>
2021-04-26 13:51:01 +0900

    20210426

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./component/ChatFeed";
 
 import "./App.css";
 
@@ -7,7 +8,12 @@ const App = () => {
   return (
     <ChatEngine
       height="100vh"
-      projectID="7ae6766d-9fb8-45cf-9fee-5074a93fa10d"
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName="admin"
+      userSecret="1234"
+      renderChatFeed={(props) => <ChatFeed {...props} />}
     />
   );
 };
+
+export default App;
